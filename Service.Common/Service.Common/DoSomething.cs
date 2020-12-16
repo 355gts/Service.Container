fc524df270ee7cd98407aba1472560ad5721cb3c
355gts <blackmore__s@hotmail.com>
2020-12-16 22:01:43 +0000

    Added a .net solution

---
+++ Service.Common/Service.Common/DoSomething.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Service.Common
+{
+    public class DoSomething : IDoSomething
+    {
+        public async Task PrintMessage(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                Console.WriteLine($"This is the {i} printed message.");
+                await Task.Delay(5000);
+            }
+        }
+    }
+}
---
+++ Service.Common/Service.Common/IDoSomething.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Service.Common
+{
+    public interface IDoSomething
+    {
+        Task PrintMessage(int times);
+    }
+}
